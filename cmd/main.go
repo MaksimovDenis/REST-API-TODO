647b10b9ea0953b78b6ccd43c1e413dba59ebf5e
Denis <maksimovis74@gmail.com>
2024-03-05 11:38:21 +0500

    db has been added

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,10 @@ import (
 	"firstRESTApi/package/repository"
 	"firstRESTApi/package/service"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
@@ -17,7 +21,23 @@ func main() {
 		log.Fatal("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPosgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatal("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- package/repository/repository.go
+++ package/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Autorisation interface {
 }
 
@@ -15,6 +17,6 @@ type Repository struct {
 	ToDoItems
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
